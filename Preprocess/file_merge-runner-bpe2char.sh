bc3bd571dbda2eadf82c7d640f8e1400f73c1221
Millie Dwyer <mmd378@nyu.edu>
2017-11-19 13:22:38 -0500

    updating char preprocessing files

--- Preprocess/file_merge-runner-bpe2char.sh
+++ Preprocess/file_merge-runner-bpe2char.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
 code_dir="/Users/millie/Documents/NLP_fall_2017/project/Preprocess_copy"
+data_dir="/Users/millie/Documents/NLP_fall_2017/project"
 
-python "$code_dir"/file_merger.py --file1 "/Users/millie/Documents/NLP_fall_2017/project/en-fr/train.en.tok.bpe" \
-                                  --file2 "/Users/millie/Documents/NLP_fall_2017/project/en-fr/train.fr.tok" \
-                                  --outdir "/Users/millie/Documents/NLP_fall_2017/project/Model2_ready/train" \
+
+
+for i in "train" "dev" "test"
+do
+        python "$code_dir"/file_merger.py --file1 "$data_dir/en-fr/"$i".en.tok.bpe" \
+                                  --file2 "$data_dir/en-fr/"$i".fr.tok" \
+                                  --outdir "$data_dir/Model2_ready/"$i \
                                   --lang1 "en" \
                                   --lang2 "fr" \
                                   --term ".bpe2char"
-                                  
+done
+
+
+
+for i in "tst2010" "tst2011" "tst2012" "tst2013" "tst2014"
+do
+        python "$code_dir"/file_merger.py --file1 "$data_dir/en-fr/"$i".en.tok.bpe" \
+                                  --file2 "$data_dir/en-fr/"$i".fr.tok" \
+                                  --outdir "$data_dir/Model2_ready/"$i \
+                                  --lang1 "en" \
+                                  --lang2 "fr" \
+                                  --term "."$i"-bpe2char"
+done
+
+
+
