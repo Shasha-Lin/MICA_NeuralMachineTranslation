97c2f9388ba8419fce5db854d363b57edea985b4
Millie Dwyer <mdwyer@nyu.edu>
2017-11-19 14:01:48 -0500

    adding unified file merger script for all models

--- Preprocess/file_merge-runner-bpe2char.sh
+++ Preprocess/file_merge-runner-bpe2char.sh
@@ -5,7 +5,7 @@ data_dir="/Users/millie/Documents/NLP_fall_2017/project"
 
 
 
-for i in "train" "dev" "test"
+for i in "train" "dev" "valid"
 do
         python "$code_dir"/file_merger.py --file1 "$data_dir/en-fr/"$i".en.tok.bpe" \
                                   --file2 "$data_dir/en-fr/"$i".fr.tok" \
@@ -21,7 +21,7 @@ for i in "tst2010" "tst2011" "tst2012" "tst2013" "tst2014"
 do
         python "$code_dir"/file_merger.py --file1 "$data_dir/en-fr/"$i".en.tok.bpe" \
                                   --file2 "$data_dir/en-fr/"$i".fr.tok" \
-                                  --outdir "$data_dir/Model2_ready/"$i \
+                                  --outdir "$data_dir/Model2_ready/test" \
                                   --lang1 "en" \
                                   --lang2 "fr" \
                                   --term "."$i"-bpe2char"
