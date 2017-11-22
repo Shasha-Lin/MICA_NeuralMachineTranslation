8c60f3d26fa3ecc87493dc18538af3823bb7385a
EDUARDO_FIERRO <eff254@nyu.edu>
2017-11-22 18:42:14 -0500

    Edus Main model with Beam Search for trials

--- EduTrials/run-main_model.sh
+++ EduTrials/run-main_model.sh
@@ -13,7 +13,4 @@ module load nltk/python3.5/3.2.4 # It is possible that you need to download nltk
 
 python3 -m pip install comet_ml --user
 
-python3 -u main_model.py --use_cuda --n_iters 800000 --learning_rate 0.0001 --teacher_forcing_ratio 1  --main_data_dir "/scratch/eff254/NLP/Data/Model_ready/" --out_dir "/scratch/eff254/NLP/ModelOutputs/"
-
-# On local machine:
-# python main_model.py --learning_rate 0.01 --main_data_dir "/Users/eduardofierro/Google Drive/TercerSemetre/NLP/ProjectOwn/Data/Model_ready" --MIN_LENGTH_INPUT 5 --MIN_LENGTH_TARGET 5 --MAX_LENGTH_INPUT 10 --MAX_LENGTH_TARGET 10 --out_dir /Users/eduardofierro/Desktop/
+python3 -u main_model_BS.py --use_cuda --n_iters 800000 --learning_rate 0.001 --teacher_forcing_ratio 0.9  --main_data_dir "/scratch/eff254/NLP/Data/Model_ready/" --out_dir "/scratch/eff254/NLP/ModelOutputs/"
