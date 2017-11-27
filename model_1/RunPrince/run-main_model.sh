62d2ca06318a5080ebb6818d9243df6e5de5aa9a
AKASHKADEL <akashkadel@live.com>
2017-11-27 18:33:09 -0500

    Making minor changes to the bash script

--- model_1/RunPrince/run-main_model.sh
+++ model_1/RunPrince/run-main_model.sh
@@ -9,7 +9,9 @@
 
 module purge
 module load python3/intel/3.5.3
-module load nltk/python3.5/3.2.4 # It is possible that you need to download nltk_data first on prince /home/ directory. 
-python3 -m pip install comet_ml --user
+module load pytorch/python3.5/0.2.0_3
+module load torchvision/python3.5/0.1.9
+module load nltk/python3.5/3.2.4 # It is possible that you need to download nltk_data first on prince /home/ directory.
 
-python3 -u main_model_BS.py --USE_CUDA --n_iters 800000 --learning_rate 0.001 --teacher_forcing_ratio 0.9  --model_type "bpe2bpe" --main_data_dir "/scratch/eff254/NLP/Data/Model_ready/" --out_dir "/scratch/eff254/NLP/ModelOutputs/"
+
+python3 -u main_model_BS.py --n_epochs 800000 --learning_rate 0.001 --teacher_forcing_ratio 0.9  --model_type "bpe2bpe" --main_data_dir "/scratch/eff254/NLP/Data/Model_ready/" --out_dir "/scratch/ak6201/NLP/ModelOutputs/"
