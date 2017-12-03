5a5020a16183f6495db66f24e6255a9cb254036f
AKASHKADEL <akashkadel@live.com>
2017-12-02 19:19:28 -0500

    bash script to run all jobs in the loop

---
+++ model_1/runningJobsAtOnce/bash_run_at_once.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### code dir contains this file and the file_merger code
+code_dir="/home/ak6201/RunPrince/main_model_BS_all.py"
+
+#for i in {1,2,3,4};
+#do
+#    echo $i
+#done
+
+learning_rate=(0.00045 0.00012 0.00072 0.00094 0.00032)
+embed_dim=(650 826 687 609 821)
+teacher_forcing_ratio=3000
+dropout=0.3
+hidden_size=(994 728 612 682 988)
+k_beam_search=(8 15 5 12 16)
+n_layers=2
+
+
+
+for (( i=0; i<5; i++));
+do
+
+    sbatch run_all_jobs.sh "bpe2bpe" "${learning_rate[$i]}" "$teacher_forcing_ratio" "${embed_dim[$i]}" "${hidden_size[$i]}" "${k_beam_search[$i]}" "$n_layers" "$code_dir"
+
+done
