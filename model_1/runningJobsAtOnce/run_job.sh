6e2568b4d9c7685758a571ef2475133b08edfdce
AKASHKADEL <akashkadel@live.com>
2017-12-02 19:20:58 -0500

    bash script to run one job

---
+++ model_1/runningJobsAtOnce/run_job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#SBATCH --job-name=mica_edu_akash
+#SBATCH --gres=gpu:1
+#SBATCH --time=40:00:00
+#SBATCH --mem=40GB
+#SBATCH --output=slurm-%A.out
+#SBATCH --error=slurm-%A.err
+
+# All the load commands below may not be required by everyone.
+# The commands below can be modified/changed/removed  based on the python version you have
+module purge
+module load python3/intel/3.5.3
+module load pytorch/python3.5/0.2.0_3
+module load torchvision/python3.5/0.1.9
+module load nltk/python3.5/3.2.4 # It is possible that you need to download nltk_data first on prince /home/ directory.
+
+model_type=$1
+learning_rate=$2
+teacher_forcing_ratio=$3
+embed_size=$4
+hidden_size=$5
+kmax=$6
+n_layers=$7
+code_dir=$8
+
+python3 -m pip install comet_ml --user
+
+python3 -u $code_dir --USE_CUDA --learning_rate $learning_rate --teacher_forcing_ratio $teacher_forcing_ratio --model_type "$model_type" --embed_size $embed_size --hidden_size $hidden_size  --kmax $kmax --main_data_dir "/scratch/eff254/NLP/Data/Model_ready/" --out_dir "/scratch/ak6201/NLP/ModelOutputs/"
