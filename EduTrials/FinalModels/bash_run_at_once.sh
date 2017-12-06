9379ec9338de1ee2efb9d2a21ebdca826499fe49
EDUARDO_FIERRO <eff254@nyu.edu>
2017-12-05 23:11:13 -0500

    Bash script modifications

--- EduTrials/FinalModels/bash_run_at_once.sh
+++ EduTrials/FinalModels/bash_run_at_once.sh
@@ -5,9 +5,9 @@ code_dir="./train.py"
 model_type="bpe2bpe"
 
 
-learning_rate=(0.00045 0.00012 0.00072 0.00094 0.00032)
+learning_rate=(0.000451 0.000121 0.000721 0.000941 0.000321)
 dropout=0.3
-hidden_size=(994 728 612 682 988)
+hidden_size=(993 729 613 683 9889)
 k_beam_search=(8 15 5 12 16)
 n_layers=2
 attention=("Bahdanau" "Luong" "Luong" "Bahdanau" "Bahdanau")
@@ -17,6 +17,6 @@ for (( i=0; i<5; i++));
 do
     experiment=$model_type$underscore${learning_rate[$i]}$underscore${hidden_size[$i]}$underscore${k_beam_search[$i]}$underscore${attention[$i]}
     
-     sbatch run_job.sh "$model_type" "${learning_rate[$i]}" "${hidden_size[$i]}" "${k_beam_search[$i]}" "$n_layers" "$code_dir" "${attention[$i]}" "$experiment"
+    sbatch run_job.sh "$model_type" "${learning_rate[$i]}" "${hidden_size[$i]}" "${k_beam_search[$i]}" "$n_layers" "$code_dir" "${attention[$i]}" "$experiment"
 
 done
